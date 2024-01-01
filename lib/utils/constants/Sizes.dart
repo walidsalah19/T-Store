3a59a4f9bdf0d0c07473792f22ac8f1caad7e64a
walid salah <walidsalah9585@gmail.com>
2024-01-01 16:26:18 +0200

    sizes

---
+++ lib/utils/constants/Sizes.dart
@@ -0,0 +1,57 @@
+class AppSizes {
+  AppSizes._();
+
+  //margin and alignment
+  static const xs = 4.0;
+  static const sm = 8.0;
+  static const md = 16.0;
+  static const lg = 24.0;
+  static const xl = 32.0;
+
+  //icons size
+  static const iconXy = 12.0;
+  static const iconSm = 16.0;
+  static const iconMd = 24.0;
+  static const iconLg = 32.0;
+
+  //fonts size
+  static const fontSm = 14.0;
+  static const fontMd = 16.0;
+  static const fontLg = 18.0;
+
+  //button size
+  static const buttonHeight = 18.0;
+  static const buttonRadius = 12.0;
+  static const buttonWidth = 180.0;
+  static const buttonElevation = 4.0;
+
+  //appBar height
+  static const appBarHeight = 56.0;
+
+  //image size
+  static const imageSize = 80.0;
+
+  //default spacing between sections
+  static const defaultSpace = 24.0;
+  static const spaceBtItems = 16.0;
+  static const spaceBtSections = 32.0;
+
+  //border radius
+  static const borderRadiusSm = 4.0;
+  static const borderRadiusMd = 8.0;
+  static const borderRadiusLg = 12.0;
+
+  //input field
+  static const inputFieldRadius = 12.0;
+  static const spaceBtInputField = 16.0;
+
+  //card size
+  static const cardRedLg = 16.0;
+  static const cardRedSm = 10.0;
+  static const cardRedMd = 12.0;
+  static const cardRedXs = 6.0;
+  static const cardElevation = 2.0;
+
+  //grid view spacing
+  static const gridViewSpace = 16.0;
+}
