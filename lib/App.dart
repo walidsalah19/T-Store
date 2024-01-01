2abe33273ade4058363fd28d37d2eec81f05171e
walid salah <walidsalah9585@gmail.com>
2024-01-01 15:46:40 +0200

    update app class

---
+++ lib/App.dart
@@ -0,0 +1,19 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:t_store/utils/themes/AppThemes.dart';
+
+class App extends StatelessWidget
+{
+  const App({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      themeMode:ThemeMode.system,
+      theme: AppThemes.lightTheme,
+      darkTheme: AppThemes.darkTheme,
+    );
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:t_store/utils/themes/AppThemes.dart';
+import 'App.dart';
 
 void main() {
   runApp(const App());
-}
-class App extends StatelessWidget
-{
-  const App({super.key});
-  @override
-  Widget build(BuildContext context) {
-   return  MaterialApp(
-     themeMode:ThemeMode.system,
-     theme: AppThemes.lightTheme,
-     darkTheme: AppThemes.darkTheme,
-   );
-  }
-
 }
