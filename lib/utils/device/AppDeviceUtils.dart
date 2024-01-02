8bdc84c44853a91297ca103909be70d6638a2f9e
walid salah <walidsalah9585@gmail.com>
2024-01-02 15:24:39 +0200

    update

--- lib/utils/device/AppDeviceUtils.dart
+++ lib/utils/device/AppDeviceUtils.dart
@@ -1,11 +1,13 @@
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class AppDeviceUtils {
   AppDeviceUtils._();
-
 
   static void hideKeyboard(BuildContext context) {
     FocusScope.of(context).requestFocus(FocusNode());
@@ -64,16 +66,51 @@ class AppDeviceUtils {
     return defaultTargetPlatform == TargetPlatform.iOS ||
         defaultTargetPlatform == TargetPlatform.android;
   }
-  static void vibrate(Duration duration)
-  {
+
+  static void vibrate(Duration duration) {
     HapticFeedback.vibrate();
-    Future.delayed(duration,() => HapticFeedback.vibrate());
+    Future.delayed(duration, () => HapticFeedback.vibrate());
   }
-  static Future<void> setPreferedOriantation(List<DeviceOrientation> oriantation) async{
+
+  static Future<void> setPreferedOriantation(
+      List<DeviceOrientation> oriantation) async {
     await SystemChrome.setPreferredOrientations(oriantation);
   }
-  static void hideStatusBar()
+
+  static void hideStatusBar() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+  }
+
+  static void showStatusBar() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: SystemUiOverlay.values);
+  }
+
+  static Future<bool> hasInternetConnection() async {
+    try {
+      final result = await InternetAddress.lookup("example.com");
+      return result.isNotEmpty && result[0].rawAddress.isNotEmpty;
+    } on SocketException catch (_) {
+      return false;
+    }
+  }
+  static bool isIos()
   {
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,overlays: SystemUiOverlay.values);
+    return Platform.isIOS;
+  }
+  static bool isAndroid()
+  {
+    return Platform.isAndroid;
+  }
+  static Future<void> lunchUr(String url) async
+  {
+    if(await canLaunchUrlString(url))
+      {
+        await lunchUr(url);
+      }
+    else
+      {
+        throw 'cant access this url $url' ;
+      }
   }
 }
