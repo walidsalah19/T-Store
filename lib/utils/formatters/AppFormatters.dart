45a53cac643d28f5ac90144f6ba8c4048b4d8675
walid salah <walidsalah9585@gmail.com>
2024-01-02 15:37:08 +0200

    update

--- lib/utils/formatters/AppFormatters.dart
+++ lib/utils/formatters/AppFormatters.dart
@@ -1,4 +1,23 @@
+import 'package:intl/intl.dart';
 
-class AppFormatters{
+class AppFormatters {
   AppFormatters._();
+
+  static String formatDate(DateTime? date) {
+    date ??= DateTime.now();
+    return DateFormat("dd-MM-yyyy").format(date);
+  }
+
+  static String formatCurrency(double amount) {
+    return NumberFormat.currency(locale: "en-us", symbol: '\$').format(amount);
+  }
+
+  static String phoneNumberFormat(String phoneNumber) {
+    if (phoneNumber.length == 10) {
+      return '(${phoneNumber.substring(0, 3)}) ${phoneNumber.substring(3, 6)} ${phoneNumber.substring(6)}';
+    }
+    return phoneNumber;
+  }
+
+
 }
