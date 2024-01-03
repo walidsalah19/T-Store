7e32e0ca31cf225af3258374316b42ddf3a3c8a2
walid salah <walidsalah9585@gmail.com>
2024-01-03 12:05:46 +0200

    update helper functions

--- lib/utils/Helper/CalculatePricing.dart
+++ lib/utils/Helper/CalculatePricing.dart
@@ -1,85 +1,29 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-
 class CalculatePricing {
-  static Color? getColors(String color) {
-    if (color == "red") {
-      return Colors.red;
-    } else if (color == "green") {
-      return Colors.green;
-    } else if (color == "blue") {
-      return Colors.blue;
-    } else if (color == "pink") {
-      return Colors.pink;
-    } else if (color == "grey") {
-      return Colors.grey;
-    } else if (color == "black") {
-      return Colors.black;
-    } else if (color == "purple") {
-      return Colors.purple;
-    } else if (color == "blue") {
-      return Colors.blue;
-    } else if (color == "indigo") {
-      return Colors.indigo;
-    } else if (color == "teal") {
-      return Colors.teal;
-    } else if (color == "brown") {
-      return Colors.brown;
-    } else if (color == "white") {
-      return Colors.white;
-    }
+  static double calculateTotalPrice(double productPrice, String location) {
+    double taxRate = getTaxForLocation(location);
+    double taxAmount = taxRate * productPrice;
+    double shippingCost = getShippingCost(location);
+    double totalPrice = productPrice * taxAmount * shippingCost;
+
+    return totalPrice;
   }
 
-  static void showSnackBar(String message) {
-    ScaffoldMessenger.of(Get.context!)
-        .showSnackBar(SnackBar(content: Text(message)));
+  static String calculateShippingCost(double productPrice, String location) {
+    double shippingCost = getShippingCost(location);
+    return shippingCost.toStringAsFixed(2);
   }
 
-  static void showAlertDialog(String title, String message) {
-    showDialog(
-      context: Get.context!,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text(title),
-          content: Text(message),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('Close'),
-            ),
-          ],
-        );
-      },
-    );
+  static String calculateTac(double productPrice, String location) {
+    double taxRate = getTaxForLocation(location);
+    double taxAmount = taxRate * productPrice;
+    return taxAmount.toStringAsFixed(2);
   }
 
-  static void navigateToScreen(BuildContext context, Widget screen) {
-    Navigator.push(context, MaterialPageRoute(builder: (_) => screen));
+  static double getTaxForLocation(String location) {
+    return .10;
   }
 
-  static String truncateText(String text, int maxLength) {
-    if (text.length <= maxLength) {
-      return text;
-    } else {
-      return '${text.substring(0, maxLength)}';
-    }
-  }
-
-  static bool isDark(BuildContext context) {
-    return Theme.of(context).brightness == Brightness.dark;
-  }
-  static Size screenSize()
-  {
-    return MediaQuery.of(Get.context!).size;
-  }
-  static double screenHeight()
-  {
-    return MediaQuery.of(Get.context!).size.height;
-  }
-  static double screenWidth()
-  {
-    return MediaQuery.of(Get.context!).size.width;
+  static double getShippingCost(String location) {
+    return 5.00;
   }
 }
--- lib/utils/Helper/HelperFunction.dart
+++ lib/utils/Helper/HelperFunction.dart
@@ -1,4 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HelperFunction{
+  static Color? getColors(String color) {
+    if (color == "red") {
+      return Colors.red;
+    } else if (color == "green") {
+      return Colors.green;
+    } else if (color == "blue") {
+      return Colors.blue;
+    } else if (color == "pink") {
+      return Colors.pink;
+    } else if (color == "grey") {
+      return Colors.grey;
+    } else if (color == "black") {
+      return Colors.black;
+    } else if (color == "purple") {
+      return Colors.purple;
+    } else if (color == "blue") {
+      return Colors.blue;
+    } else if (color == "indigo") {
+      return Colors.indigo;
+    } else if (color == "teal") {
+      return Colors.teal;
+    } else if (color == "brown") {
+      return Colors.brown;
+    } else if (color == "white") {
+      return Colors.white;
+    }
+  }
 
+  static void showSnackBar(String message) {
+    ScaffoldMessenger.of(Get.context!)
+        .showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  static void showAlertDialog(String title, String message) {
+    showDialog(
+      context: Get.context!,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(message),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Close'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  static void navigateToScreen(BuildContext context, Widget screen) {
+    Navigator.push(context, MaterialPageRoute(builder: (_) => screen));
+  }
+
+  static String truncateText(String text, int maxLength) {
+    if (text.length <= maxLength) {
+      return text;
+    } else {
+      return '${text.substring(0, maxLength)}';
+    }
+  }
+
+  static bool isDark(BuildContext context) {
+    return Theme.of(context).brightness == Brightness.dark;
+  }
+  static Size screenSize()
+  {
+    return MediaQuery.of(Get.context!).size;
+  }
+  static double screenHeight()
+  {
+    return MediaQuery.of(Get.context!).size.height;
+  }
+  static double screenWidth()
+  {
+    return MediaQuery.of(Get.context!).size.width;
+  }
 }
