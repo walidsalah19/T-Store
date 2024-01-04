2d9c0c1519c77969ff5d39e1c69474c46ce42a15
walid salah <walidsalah9585@gmail.com>
2024-01-04 10:05:15 +0200

    update

--- lib/App.dart
+++ lib/App.dart
@@ -14,6 +14,7 @@ class App extends StatelessWidget
       themeMode:ThemeMode.system,
       theme: AppThemes.lightTheme,
       darkTheme: AppThemes.darkTheme,
+      home:Scaffold()
     );
   }
 
